3efb41c090af1331bf76ce0b440d7c3233ea1b9a
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-21 20:10:56 +0530

    Added:Determined length of line

--- LineComparison/src/HelloMessage.java
+++ LineComparison/src/HelloMessage.java
@@ -1,6 +1,13 @@
-
+import java.util.Scanner;
 public class HelloMessage {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+		System.out.println("Enter x and y coordinates");
+		Scanner myObj = new Scanner(System.in);
+		int x1 = myObj.nextInt();
+		int y1 = myObj.nextInt();
+		int x2 = myObj.nextInt();
+		int y2 = myObj.nextInt();
+		double length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		System.out.println("The length of the line is " +length);
 	}
 }
